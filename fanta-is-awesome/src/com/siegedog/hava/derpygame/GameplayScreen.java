67bed43280dbbed326cf5df5c87305f573b5470b
Cristian Baciu <crist.baciu@yahoo.com>
2011-12-18 20:32:47 +0200

    chase

--- fanta-is-awesome/src/com/siegedog/hava/derpygame/GameplayScreen.java
+++ fanta-is-awesome/src/com/siegedog/hava/derpygame/GameplayScreen.java
@@ -28,206 +28,206 @@ import com.siegedog.hava.engine.Unit;
 
 public class GameplayScreen implements Screen {
 
-	public final int PIXELS_PER_METER = 32;
+ public final int PIXELS_PER_METER = 32;
 
-	private static final int[] layersList = { 2, 3 };
+ private static final int[] layersList = { 2, 3 };
 
-	FancyGame game;
-	SpriteBatch spriteBatch;
-	BitmapFont font;
+ FancyGame game;
+ SpriteBatch spriteBatch;
+ BitmapFont font;
 
-	Stage stage;
-	Sprite TS;
+ Stage stage;
+ Sprite TS;
 
-	GameCam2D cam;
-	OrthographicCamera UICam;
+ GameCam2D cam;
+ OrthographicCamera UICam;
 
-	GameInput gameInput;
+ GameInput gameInput;
 
-	Node root = new Node("root");
-	Unit dude1;
+ Node root = new Node("root");
+ Unit dude1;
 
-	long startTime = System.nanoTime();
-	Vector3 tmp = new Vector3();
+ long startTime = System.nanoTime();
+ Vector3 tmp = new Vector3();
 
-	TileMap map;
-	float gravityStrength = 80f;
-	
-	
+ TileMap map;
+ float gravityStrength = 80f;
+ 
+ 
 
-	World world;
-	Box2DDebugRenderer boxDebugRenderer;
-	
-	public void initPlayer(Unit player) {
-		dude1 = player;
-		root.addNode(player);
-	}
+ World world;
+ Box2DDebugRenderer boxDebugRenderer;
+ 
+ public void initPlayer(Unit player) {
+  dude1 = player;
+  root.addNode(player);
+ }
 
-	public GameplayScreen(FancyGame game) {
-		this.game = game;
+ public GameplayScreen(FancyGame game) {
+  this.game = game;
 
-		GameplayScreen.instance = this;
+  GameplayScreen.instance = this;
 
-		font = new BitmapFont();
-		font.setColor(new Color(0.5f, 0.5f, 0.8f, 0.94f));
+  font = new BitmapFont();
+  font.setColor(new Color(0.5f, 0.5f, 0.8f, 0.94f));
 
-		spriteBatch = new SpriteBatch();
+  spriteBatch = new SpriteBatch();
 
-		// World setup
-		world = new World(new Vector2(0, -gravityStrength), true);
+  // World setup
+  world = new World(new Vector2(0, -gravityStrength), true);
 
-		map = new TileMap("level", PIXELS_PER_METER, this);
-		map.loadCollisions("data/tiledmap/collisions.txt", world);
-		boxDebugRenderer = new Box2DDebugRenderer(true, true, true);
-		
-		cam = 	new GameCam2D(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 
-				new Vector2(map.getWidth() * PIXELS_PER_METER, map.getHeight() * PIXELS_PER_METER));
-		
-		cam.follow(dude1.getPhysics(), false);
-		
-		root.addNode(new UberJumpNode(15,40,3,3));
-		root.addNode(new UberSpeedNode(17,43,3,3));
-		
-		RetardedAI retAI = new RetardedAI();
-		AIInputNode pedoInput = new AIInputNode((LRR)dude1, retAI);
-		PedoBear pedo = new PedoBear(17*PIXELS_PER_METER,44*PIXELS_PER_METER, pedoInput);
-		root.addNode(pedo);
+  map = new TileMap("level", PIXELS_PER_METER, this);
+  map.loadCollisions("data/tiledmap/collisions.txt", world);
+  boxDebugRenderer = new Box2DDebugRenderer(true, true, true);
+  
+  cam =  new GameCam2D(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 
+    new Vector2(map.getWidth() * PIXELS_PER_METER, map.getHeight() * PIXELS_PER_METER));
+  
+  cam.follow(dude1.getPhysics(), false);
+  
+  root.addNode(new UberJumpNode(15,40,3,3));
+  root.addNode(new UberSpeedNode(17,43,3,3));
+  
+  RetardedAI retAI = new RetardedAI();
+  AIInputNode pedoInput = new AIInputNode((LRR)dude1, retAI);
+  PedoBear pedo = new PedoBear(17*PIXELS_PER_METER,44*PIXELS_PER_METER, pedoInput);
+  root.addNode(pedo);
 
-		// So far, just a thingy to let us drag the world around
-		//gameInput = new GameInput(game, this);
-		//Gdx.input.setInputProcessor(gameInput);
+  // So far, just a thingy to let us drag the world around
+  //gameInput = new GameInput(game, this);
+  //Gdx.input.setInputProcessor(gameInput);
 
-		/*
-		RenderNode2D aniTest = new RenderNode2D("data/img/sprites/penis.png",
-				32, 32);
-	
-		aniTest.getSprite().setPosition(15 * PIXELS_PER_METER,
-				40 * PIXELS_PER_METER);
-		root.addNode(aniTest);*/
-		 
-		
-		// BasicSaveData save = new
-		// SaveGameHelper<BasicSaveData>().readSaveData("save.sav");
-		// System.out.println("Loaded game...");
-		// System.out.println(save.name);
+  /*
+  RenderNode2D aniTest = new RenderNode2D("data/img/sprites/penis.png",
+    32, 32);
+ 
+  aniTest.getSprite().setPosition(15 * PIXELS_PER_METER,
+    40 * PIXELS_PER_METER);
+  root.addNode(aniTest);*/
+   
+  
+  // BasicSaveData save = new
+  // SaveGameHelper<BasicSaveData>().readSaveData("save.sav");
+  // System.out.println("Loaded game...");
+  // System.out.println(save.name);
 
-		Resources.loadSfx("data/sound/sfx/");
+  Resources.loadSfx("data/sound/sfx/");
 
-	}
+ }
 
-	@Override
-	public void render(float delta) { // called at 60fps
-		Gdx.gl.glClearColor(0.1f, 0.1f, 0.3f, 1f);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+ @Override
+ public void render(float delta) { // called at 60fps
+  Gdx.gl.glClearColor(0.1f, 0.1f, 0.3f, 1f);
+  Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
-		// Async loading
-		Resources.update(delta);
+  // Async loading
+  Resources.update(delta);
 
-		// Do the physics
-		world.step(delta, 8, 3);
+  // Do the physics
+  world.step(delta, 8, 3);
 
-		// And then the logic
-		root.update(delta);
-		// stage.act(delta);
+  // And then the logic
+  root.update(delta);
+  // stage.act(delta);
 
-		// Cam should always be updated.
-		// It's not expensive and prevents a bunch of bugs.
-		cam.update(delta);
+  // Cam should always be updated.
+  // It's not expensive and prevents a bunch of bugs.
+  cam.update(delta);
 
-		// And render everything (if we can)
-		renderGame(delta);
-	}
+  // And render everything (if we can)
+  renderGame(delta);
+ }
 
-	private void renderGame(float delta) {
-		// Start rendering shit. map first.
-		map.render(cam.getGameCam());
+ private void renderGame(float delta) {
+  // Start rendering shit. map first.
+  map.render(cam.getGameCam());
 
-		spriteBatch.setProjectionMatrix(cam.getGameCam().combined);
-		spriteBatch.begin();
+  spriteBatch.setProjectionMatrix(cam.getGameCam().combined);
+  spriteBatch.begin();
 
-		// TS.draw(spriteBatch);
-		// stage.draw();
-		Node.renderAllNodes(spriteBatch);
-		spriteBatch.end();
+  // TS.draw(spriteBatch);
+  // stage.draw();
+  Node.renderAllNodes(spriteBatch);
+  spriteBatch.end();
 
-		if (Gdx.app.getType() == Application.ApplicationType.Desktop) {
-			Matrix4 m = new Matrix4(cam.getGameCam().combined);
-			m.scale(PIXELS_PER_METER, PIXELS_PER_METER, PIXELS_PER_METER);
-			boxDebugRenderer.render(world, m);
-		}
+  if (Gdx.app.getType() == Application.ApplicationType.Desktop) {
+   Matrix4 m = new Matrix4(cam.getGameCam().combined);
+   m.scale(PIXELS_PER_METER, PIXELS_PER_METER, PIXELS_PER_METER);
+   boxDebugRenderer.render(world, m);
+  }
 
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		spriteBatch.setProjectionMatrix(cam.getUICam().combined);
-		spriteBatch.begin();
-		font.draw(
-				spriteBatch,
-				"FPS: " + Gdx.graphics.getFramesPerSecond() + " World: "
-						+ world.getBodyCount() + " bodies." + "\n"
-						+ dude1.getBoxDebug(), cam.getUICam().position.x - w
-						/ 2 + 40, cam.getUICam().position.y - h / 2 + 40);
+  float w = Gdx.graphics.getWidth();
+  float h = Gdx.graphics.getHeight();
+  spriteBatch.setProjectionMatrix(cam.getUICam().combined);
+  spriteBatch.begin();
+  font.draw(
+    spriteBatch,
+    "FPS: " + Gdx.graphics.getFramesPerSecond() + " World: "
+      + world.getBodyCount() + " bodies." + "\n"
+      + dude1.getBoxDebug(), cam.getUICam().position.x - w
+      / 2 + 40, cam.getUICam().position.y - h / 2 + 40);
 
-		tmp.set(0, 0, 0);
-		cam.getGameCam().unproject(tmp);
-		font.draw(spriteBatch, "DUDE location: " + dude1.getPhysics().getBody().getPosition(), 20, 80);
-		spriteBatch.end();
-	}
+  tmp.set(0, 0, 0);
+  cam.getGameCam().unproject(tmp);
+  font.draw(spriteBatch, "DUDE location: " + dude1.getPhysics().getBody().getPosition(), 20, 80);
+  spriteBatch.end();
+ }
 
-	@Override
-	public void resize(int width, int height) {
-		cam.resize(width, height);
+ @Override
+ public void resize(int width, int height) {
+  cam.resize(width, height);
 
-	}
+ }
 
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
+ @Override
+ public void show() {
+  // TODO Auto-generated method stub
 
-	}
+ }
 
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
+ @Override
+ public void hide() {
+  // TODO Auto-generated method stub
 
-	}
+ }
 
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
+ @Override
+ public void pause() {
+  // TODO Auto-generated method stub
 
-	}
+ }
 
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
+ @Override
+ public void resume() {
+  // TODO Auto-generated method stub
 
-	}
+ }
 
-	@Override
-	public void dispose() {
-		instance = null;
-	}
+ @Override
+ public void dispose() {
+  instance = null;
+ }
 
-	public GameCam2D getCam() {
-		return cam;
-	}
+ public GameCam2D getCam() {
+  return cam;
+ }
 
-	public World getWorld() {
-		return world;
-	}
-	
-	public InputNode getPlayerInputNode() {
-		// pff hack lol
-		return new KeyboardInputNode();
-	}
-	
-	public Node getRoot() {
-		return root;
-	}
+ public World getWorld() {
+  return world;
+ }
+ 
+ public InputNode getPlayerInputNode() {
+  // pff hack lol
+  return new KeyboardInputNode();
+ }
+ 
+ public Node getRoot() {
+  return root;
+ }
 
-	private static GameplayScreen instance;
+ private static GameplayScreen instance;
 
-	public static GameplayScreen get() {
-		return instance;
-	}
+ public static GameplayScreen get() {
+  return instance;
+ }
 }
--- fanta-is-awesome/src/com/siegedog/hava/engine/GameCam2D.java
+++ fanta-is-awesome/src/com/siegedog/hava/engine/GameCam2D.java
@@ -10,197 +10,197 @@ import com.badlogic.gdx.scenes.scene2d.Interpolator;
 
 public class GameCam2D {
 
-	Vector3 current = new Vector3();
-	Vector3 last = new Vector3(-1, -1, -1);
-	Vector3 delta = new Vector3();
+ Vector3 current = new Vector3();
+ Vector3 last = new Vector3(-1, -1, -1);
+ Vector3 delta = new Vector3();
 
-	int width, height;
+ int width, height;
 
-	final float ppm = 32.0f;
+ final float ppm = 32.0f;
 
-	PlatformerEntityNode following;
+ PlatformerEntityNode following;
 
-	float minZoom = 0.4f;
-	float maxZoom = 40f;
-	float targetZoom = 1f;
-	float zoomStep = 0.01f;
+ float minZoom = 0.4f;
+ float maxZoom = 40f;
+ float targetZoom = 1f;
+ float zoomStep = 0.01f;
 
-	Vector2 maxPos;
-	
-	OrthographicCamera gameCam;
-	OrthographicCamera UICam;
-	
-	boolean useDynamicZoom = false;
+ Vector2 maxPos;
+ 
+ OrthographicCamera gameCam;
+ OrthographicCamera UICam;
+ 
+ boolean useDynamicZoom = false;
 
-	Vector3 targetPosition;
-	float panStep = 25f;
+ Vector3 targetPosition;
+ float panStep = 25f;
 
-	long lastTap = System.currentTimeMillis();
-	Vector2 lastTapPos;
-	float maxPosDelta = 10f;
+ long lastTap = System.currentTimeMillis();
+ Vector2 lastTapPos;
+ float maxPosDelta = 10f;
 
-	Vector3 diff;
+ Vector3 diff;
 
-	Interpolator interpolator;
+ Interpolator interpolator;
 
-	public GameCam2D(int width, int height, Vector2 maxPos) {
-		gameCam = new OrthographicCamera(width, height);
-		UICam = new OrthographicCamera(width, height);
-		
-		this.maxPos = maxPos;
-		maxPos.x /= 2;
-		maxPos.y /= 2;
-		
-		UICam.zoom = 1f;
-		gameCam.zoom = 0.5f;
+ public GameCam2D(int width, int height, Vector2 maxPos) {
+  gameCam = new OrthographicCamera(width, height);
+  UICam = new OrthographicCamera(width, height);
+  
+  this.maxPos = maxPos;
+  maxPos.x /= 2;
+  maxPos.y /= 2;
+  
+  UICam.zoom = 1f;
+  gameCam.zoom = 0.5f;
 
-		this.width = width;
-		this.height = height;
-		lastTapPos = new Vector2(-1, -1);
-		targetPosition = new Vector3(width / 2, height / 2, 0);
+  this.width = width;
+  this.height = height;
+  lastTapPos = new Vector2(-1, -1);
+  targetPosition = new Vector3(width / 2, height / 2, 0);
 
-		diff = new Vector3();
-	}
+  diff = new Vector3();
+ }
 
-	public void drag(int x, int y) {
-		gameCam.unproject(current.set(x, y, 0));
+ public void drag(int x, int y) {
+  gameCam.unproject(current.set(x, y, 0));
 
-		if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
-			// If we're in the middle of a drag (employ delta)
+  if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
+   // If we're in the middle of a drag (employ delta)
 
-			gameCam.unproject(delta.set(last.x, last.y, 0)); // Convert delta
-																// into world
-			// space
-			delta.sub(current); // And actually do the delta computation (in
-								// world space???)
-			gameCam.position.add(delta); // Oh we need world space for the cam
-											// movement
-			targetPosition.set(gameCam.position);
-		}
+   gameCam.unproject(delta.set(last.x, last.y, 0)); // Convert delta
+                // into world
+   // space
+   delta.sub(current); // And actually do the delta computation (in
+        // world space???)
+   gameCam.position.add(delta); // Oh we need world space for the cam
+           // movement
+   targetPosition.set(gameCam.position);
+  }
 
-		last.set(x, y, 0);
-	}
+  last.set(x, y, 0);
+ }
 
-	public void tap(int x, int y) {
-		last.set(-1, -1, -1);
-		Vector2 tapPos = new Vector2(x, y);
+ public void tap(int x, int y) {
+  last.set(-1, -1, -1);
+  Vector2 tapPos = new Vector2(x, y);
 
-		long currentTap = System.currentTimeMillis();
-		if (currentTap - lastTap < 300
-				&& (tapPos.sub(lastTapPos).len() < maxPosDelta || (lastTapPos.x == -1f && lastTapPos.y == -1f))) {
-			if (gameCam.zoom < 2f) {
-				targetZoom = maxZoom * 0.75f;
-				gameCam.unproject(targetPosition.set(width / 2, height / 2, 0));
-			} else {
-				// TODO: cubic interpolation
-				// TODO: estimate which interpolation will take longer
-				// and ellongate the other to match
+  long currentTap = System.currentTimeMillis();
+  if (currentTap - lastTap < 300
+    && (tapPos.sub(lastTapPos).len() < maxPosDelta || (lastTapPos.x == -1f && lastTapPos.y == -1f))) {
+   if (gameCam.zoom < 2f) {
+    targetZoom = maxZoom * 0.75f;
+    gameCam.unproject(targetPosition.set(width / 2, height / 2, 0));
+   } else {
+    // TODO: cubic interpolation
+    // TODO: estimate which interpolation will take longer
+    // and ellongate the other to match
 
-				targetZoom = minZoom * 1.75f;
-				gameCam.unproject(targetPosition.set(x, y, 0));
-			}
+    targetZoom = minZoom * 1.75f;
+    gameCam.unproject(targetPosition.set(x, y, 0));
+   }
 
-		}
-		lastTap = currentTap;
-		lastTapPos = tapPos;
-	}
+  }
+  lastTap = currentTap;
+  lastTapPos = tapPos;
+ }
 
-	public void zoom(int amount) {
-		gameCam.zoom += amount / 7f;
-		gameCam.zoom = MU.clamp(gameCam.zoom, minZoom, maxZoom);
+ public void zoom(int amount) {
+  gameCam.zoom += amount / 7f;
+  gameCam.zoom = MU.clamp(gameCam.zoom, minZoom, maxZoom);
 
-		targetZoom = gameCam.zoom;
-	}
+  targetZoom = gameCam.zoom;
+ }
 
-	public Matrix4 getUIMatrix() {
-		return gameCam.combined;
-	}
+ public Matrix4 getUIMatrix() {
+  return gameCam.combined;
+ }
 
-	public Vector2 get2DCoords() {
-		return new Vector2(gameCam.position.x - width / 2 * gameCam.zoom, gameCam.position.y - height
-				/ 2 * gameCam.zoom);
-	}
+ public Vector2 get2DCoords() {
+  return new Vector2(gameCam.position.x - width / 2 * gameCam.zoom, gameCam.position.y - height
+    / 2 * gameCam.zoom);
+ }
 
-	// TODO: read up on <Observer> pattern
-	public void follow(PlatformerEntityNode target, boolean jump) {
-		following = target;
-		if (jump) {
-			Vector2 pos = following.getBody().getPosition();
-			gameCam.position.set(targetPosition.set(pos.x * ppm, pos.y * ppm, 0));
-		}
-	}
-	
-	public OrthographicCamera getGameCam(){
-		return gameCam;
-	}
-	
-	// TODO: implement
-	public OrthographicCamera getUICam() {
-		//UICam.position.set(gameCam.position);
-		UICam.update();
-		return UICam;
-	}
-	
-	public void resize(float w, float h) {
-		Vector3 oldPos = gameCam.position;
-		gameCam.setToOrtho(false, w, h);
-		UICam.setToOrtho(false, w, h);
-		gameCam.position.set(oldPos);
-	}
+ // TODO: read up on <Observer> pattern
+ public void follow(PlatformerEntityNode target, boolean jump) {
+  following = target;
+  if (jump) {
+   Vector2 pos = following.getBody().getPosition();
+   gameCam.position.set(targetPosition.set(pos.x * ppm, pos.y * ppm, 0));
+  }
+ }
+ 
+ public OrthographicCamera getGameCam(){
+  return gameCam;
+ }
+ 
+ // TODO: implement
+ public OrthographicCamera getUICam() {
+  //UICam.position.set(gameCam.position);
+  UICam.update();
+  return UICam;
+ }
+ 
+ public void resize(float w, float h) {
+  Vector3 oldPos = gameCam.position;
+  gameCam.setToOrtho(false, w, h);
+  UICam.setToOrtho(false, w, h);
+  gameCam.position.set(oldPos);
+ }
 
-	/*
-	 * public void follow(Node node) throws Exception { Object res =
-	 * node.qc(BoxNode.class); if(res != null) this.following = (BoxNode)res;
-	 * else throw new Exception("Node " + node.name +
-	 * " doesn't have a followable component."); }
-	 */
+ /*
+  * public void follow(Node node) throws Exception { Object res =
+  * node.qc(BoxNode.class); if(res != null) this.following = (BoxNode)res;
+  * else throw new Exception("Node " + node.name +
+  * " doesn't have a followable component."); }
+  */
 
-	public void update(float delta) {
+ public void update(float delta) {
 
-		// Update zoom
-		if(useDynamicZoom) {
-			if (Math.abs(gameCam.zoom - targetZoom) < zoomStep)
-				gameCam.zoom = targetZoom;
-			else {
-				if (gameCam.zoom < targetZoom)
-					gameCam.zoom += zoomStep;
-				else if (gameCam.zoom > targetZoom)
-					gameCam.zoom -= zoomStep;
-			}
-		}
+  // Update zoom
+  if(useDynamicZoom) {
+   if (Math.abs(gameCam.zoom - targetZoom) < zoomStep)
+    gameCam.zoom = targetZoom;
+   else {
+    if (gameCam.zoom < targetZoom)
+     gameCam.zoom += zoomStep;
+    else if (gameCam.zoom > targetZoom)
+     gameCam.zoom -= zoomStep;
+   }
+  }
 
-		// Update position
-		if (following != null) {
-			Body body = following.getBody();
-			Vector2 tmp = new Vector2(body.getPosition());
-			// unproject(position.set(tmp.x, tmp.y, 0));
-			targetPosition.set(tmp.x * ppm, tmp.y * ppm, 1);
+  // Update position
+  if (following != null) {
+   Body body = following.getBody();
+   Vector2 tmp = new Vector2(body.getPosition());
+   // unproject(position.set(tmp.x, tmp.y, 0));
+   targetPosition.set(tmp.x * ppm, tmp.y * ppm, 1);
 
-			// Dynamic zoom
-			targetZoom = 0.8f + body.getLinearVelocity().len()
-					/ following.getMaxHspeed() * 0.4f;
-		}
+   // Dynamic zoom
+   targetZoom = 0.8f + body.getLinearVelocity().len()
+     / following.getMaxHspeed() * 0.4f;
+  }
 
-		diff.set(targetPosition).sub(gameCam.position);
-		float dist = diff.len();
-		float realStep = (dist/30f) * (dist/30f);
-		if (dist < realStep) {
-			gameCam.position.set(targetPosition);
-		} else {
-			Vector3 pan = new Vector3(diff.nor().mul( /*(dist/ 30f) **/ realStep ));
-			gameCam.position.add(pan);
-		}
-		
-		Vector2 cPos = new Vector2(gameCam.position.x, gameCam.position.y);
-		Vector2 min = new Vector2(0f, 0f);
-		Vector2 max = new Vector2(maxPos);
-		min.add(width /2 * gameCam.zoom, height /2 * gameCam.zoom);
-		max.sub(width /2 * gameCam.zoom, height /2 *gameCam.zoom);
-		
-		MU.clamp(cPos, min, max);
-		gameCam.position.set(cPos.x, cPos.y, gameCam.position.z);
+  diff.set(targetPosition).sub(gameCam.position);
+  float dist = diff.len();
+  float realStep = (dist/30f) * (dist/30f);
+  if (dist < realStep) {
+   gameCam.position.set(targetPosition);
+  } else {
+   Vector3 pan = new Vector3(diff.nor().mul( /*(dist/ 30f) **/ realStep ));
+   gameCam.position.add(pan);
+  }
+  
+  Vector2 cPos = new Vector2(gameCam.position.x, gameCam.position.y);
+  Vector2 min = new Vector2(0f, 0f);
+  Vector2 max = new Vector2(maxPos);
+  min.add(width /2 * gameCam.zoom, height /2 * gameCam.zoom);
+  max.sub(width /2 * gameCam.zoom, height /2 *gameCam.zoom);
+  
+  MU.clamp(cPos, min, max);
+  gameCam.position.set(cPos.x, cPos.y, gameCam.position.z);
 
-		gameCam.update();
-		UICam.update();
-	}
+  gameCam.update();
+  UICam.update();
+ }
 }
